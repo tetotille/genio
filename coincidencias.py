9e9cfb7350a801acc930e82610cd6ae5dd315c41
Jorge Tillería <62524602+tetotille@users.noreply.github.com>
2020-03-25 23:56:50 -0400

    Add files via upload

---
+++ coincidencias.py
@@ -0,0 +1,12 @@
+def contarCoincidencias(texto, lista_palabras):
+    for pal in lista_palabras:
+        b = 0
+        letras = len(pal)
+        cont = -1
+        acu = 0
+
+        while b !=-1:
+            b = texto[acu:len(texto)].find(pal)
+            cont+=1
+            acu+=b+1
+        print(pal+" = "+str(cont))
---
+++ main.py
@@ -0,0 +1,45 @@
+from procesamiento import procesar
+from time import time
+from search import search
+from coincidencias import contarCoincidencias
+
+start = time()
+########################################PROCESAMIENTO DE IMAGEN#########################
+titulo_string, lista_palabras=procesar("imagen4.jpeg")
+
+########################################PALABRA CLAVE##############################
+#falta agregar una función que elija lo que haya que buscar
+a = titulo_string.find('"')
+if a != -1:
+    a2 = titulo_string[a+1:-1].find('"')+a
+    palabra_clave = titulo_string[a+1:a2]
+elif not titulo_string[2:-1].islower():
+    aux = ""
+    for i in range(len(titulo_string[2:-1])):
+        letra = titulo_string[i+2]
+        if letra.isupper():
+            j = i
+            i = titulo_string.find(" ")
+            aux = aux+titulo_string[j:i]
+    palabra_clave = aux
+elif "el" in titulo_string.lower() and "es":
+    a = titulo_string.lower().find("el")
+    b = titulo_string.find("es")
+    palabra_clave = titulo_string[a+3:b-1]
+else:
+    palabra_clave = titulo_string
+########################################BUSQUEDA####################################
+
+titulo_string = titulo_string.lower()
+resultados = search(palabra_clave, 1)
+definitivo = ""
+for resul in resultados:
+    definitivo = definitivo+resul.description
+
+##########################################COINCIDENCIAS##############################
+
+texto = definitivo.lower()
+contarCoincidencias(texto, lista_palabras)
+
+#########################################FIN########################################
+print(time()-start)
