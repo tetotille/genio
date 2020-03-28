266ebaf4301506f8b2ef44b467e4415f764b01c6
alecaceres <alejandro.caceres@wardenautomation.com>
2020-03-28 11:55:17 -0400

    Eliminé espacios al importar una biblioteca

--- procesamiento.py
+++ procesamiento.py
@@ -1,5 +1,5 @@
 from __future__ import division   # fuerza aritmética no entera
-from PIL import Image                      # funciones para cargar y manipular imágenes
+from PIL import Image             # funciones para cargar y manipular imágenes
 import numpy as np                # funciones numéricas (arrays, matrices, etc.)
 import matplotlib.pyplot as plt   # funciones para representación gráfica
 import pytesseract
@@ -11,7 +11,7 @@ def formatoProcesamiento(imagen):
     #I1.show()
     a = np.asarray(imagen,dtype=np.float32)  # convierte el objeto I1 en una matriz de tipo float32
     return a
-    
+
 def formatoImpresion(imagen):
     return Image.fromarray(imagen.astype(np.uint8))
 ##Para guardar
@@ -34,9 +34,9 @@ def procesar(direccion):
     pytesseract.pytesseract.tesseract_cmd = r'C:\Users\teto_\AppData\Local\Tesseract-OCR\tesseract.exe'
 
     titulo_string = pytesseract.image_to_string(titulo)
-    if titulo_string[0]=="é": 
+    if titulo_string[0]=="é":
         titulo_string=" "+titulo_string[1:-1]+" "
-        
+
     primera_string = pytesseract.image_to_string(primera_opcion)
     primera_string=primera_string.lower()
 
@@ -51,7 +51,7 @@ def procesar(direccion):
     #segunda_opcion.show()
     #tercera_opcion.show()
     #print(titulo_string+"\n"+primera_string+"\n"+segunda_string+"\n"+tercera_string)
-    
+
     pregunta = titulo_string
     respuestas = set()
     respuestas.add(primera_string)
