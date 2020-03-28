11b26dc397cf468e74245d306a4efeee155f2d51
tetotille <tetotille@gmail.com>
2020-03-28 17:43:53 -0400

    Modificación del mein y procesamiento

--- main.py
+++ main.py
@@ -2,44 +2,97 @@ from procesamiento import procesar
 from time import time
 from search import search
 from coincidencias import contarCoincidencias
+import pyautogui
+import os
 
-start = time()
+
+
+
+
 ########################################PROCESAMIENTO DE IMAGEN#########################
-titulo_string, lista_palabras=procesar("imagen4.jpeg")
-
+def procesamientoImagen():
+    screenshot = pyautogui.screenshot(region=(1089, 210, 277, 440))
+    titulo_string, lista_palabras=procesar(screenshot)
+    return titulo_string,lista_palabras
 ########################################PALABRA CLAVE##############################
 #falta agregar una función que elija lo que haya que buscar
-a = titulo_string.find('"')
-if a != -1:
-    a2 = titulo_string[a+1:-1].find('"')+a
-    palabra_clave = titulo_string[a+1:a2]
-elif not titulo_string[2:-1].islower():
-    aux = ""
-    for i in range(len(titulo_string[2:-1])):
-        letra = titulo_string[i+2]
-        if letra.isupper():
-            j = i
-            i = titulo_string.find(" ")
-            aux = aux+titulo_string[j:i]
-    palabra_clave = aux
-elif "el" in titulo_string.lower() and "es":
-    a = titulo_string.lower().find("el")
-    b = titulo_string.find("es")
-    palabra_clave = titulo_string[a+3:b-1]
-else:
-    palabra_clave = titulo_string
+def palabraClave(titulo_string):
+    a = titulo_string.find('"')
+    if a != -1:
+        a2 = titulo_string[a+1:-1].find('"')+a
+        palabra_clave = titulo_string[a+1:a2]
+    elif not titulo_string[2:-1].islower():
+        aux = ""
+        for i in range(len(titulo_string[2:-1])):
+            letra = titulo_string[i+2]
+            if letra.isupper():
+                j = i
+                i = titulo_string.find(" ")
+                aux = aux+titulo_string[j:i]
+        palabra_clave = aux
+    elif "el" in titulo_string.lower() and "es":
+        a = titulo_string.lower().find("el")
+        b = titulo_string.find("es")
+        palabra_clave = titulo_string[a+3:b-1]
+    else:
+        palabra_clave = titulo_string
+    return palabra_clave
 ########################################BUSQUEDA####################################
-
-titulo_string = titulo_string.lower()
-resultados = search(palabra_clave, 1)
-definitivo = ""
-for resul in resultados:
-    definitivo = definitivo+resul.description
+def busqueda(titulo_string,palabra_clave):
+    resultados = search(palabra_clave, 1)
+    definitivo = ""
+    for resul in resultados:
+        definitivo = definitivo+resul.description
+    return definitivo
 
 ##########################################COINCIDENCIAS##############################
-
-texto = definitivo.lower()
-contarCoincidencias(texto, lista_palabras)
+def coincidencias(definitivo,lista_palabras):
+    texto = definitivo.lower()
+    contarCoincidencias(texto, lista_palabras)
 
 #########################################FIN########################################
-print("Tardó mucho nomás", time()-start)
+
+
+
+def busquedaPregunta():
+    titulo_string,lista_palabras = procesamientoImagen()
+    palabra_clave=palabraClave(titulo_string)
+    definitivo = busqueda(titulo_string,palabra_clave)
+    coincidencias(definitivo,lista_palabras)
+
+def busquedaImagen():
+    screenshot = pyautogui.screenshot(region=(1089, 210, 277, 440))
+    definitivo=googleImages(screenshot)#falta implementar
+    input("\npresione para hacer screenshot\n")
+    titulo_string,lista_palabras = procesamientoImagen()
+    coincidencias(definitivo,lista_palabras)
+
+def busquedaShazam():
+    palabra_clave = input("\nEscriba la palabra clave\n")
+    try:
+        titulo_string,lista_palabras = procesamientoImagen()
+    except:
+        pass
+    definitivo = busqueda(titulo_string,palabra_clave)
+    print(definitivo)
+    coincidencias(definitivo,lista_palabras)
+    
+#start = time()
+def main():
+    while(True):
+        try:
+            n = int(input("Bienvenido señor, espero órdenes"))
+        except:
+            continue
+        if n == 0:
+            os.system("cls")
+            input("Hasta luego señor")
+            break
+        elif n == 1:
+            busquedaPregunta()
+        elif n == 2:
+            busquedaImagen()
+        elif n == 3:
+            busquedaShazam()
+        input()
+        os.system("cls")
--- procesamiento.py
+++ procesamiento.py
@@ -1,9 +1,14 @@
 from __future__ import division   # fuerza aritmética no entera
-from PIL import Image             # funciones para cargar y manipular imágenes
+from PIL import Image                      # funciones para cargar y manipular imágenes
 import numpy as np                # funciones numéricas (arrays, matrices, etc.)
 import matplotlib.pyplot as plt   # funciones para representación gráfica
 import pytesseract
 from time import time
+from PIL import ImageFilter
+
+import pyocr
+import pyocr.builders
+import sys
 
 ####################################PROCESAMIENTO DE IMAGEN###################################
 def formatoProcesamiento(imagen):
@@ -11,13 +16,13 @@ def formatoProcesamiento(imagen):
     #I1.show()
     a = np.asarray(imagen,dtype=np.float32)  # convierte el objeto I1 en una matriz de tipo float32
     return a
-
+    
 def formatoImpresion(imagen):
     return Image.fromarray(imagen.astype(np.uint8))
 ##Para guardar
 #I.save("Nombre.jpg")
 ##################MAIN################
-def procesar(direccion):
+def procesar2(direccion):
     I = Image.open(direccion)
 
     I = formatoProcesamiento(I)
@@ -27,16 +32,16 @@ def procesar(direccion):
     tercera_opcion = I[751:849,44:493]
 
     titulo=formatoImpresion(titulo)
-    primera_opcion=formatoImpresion(primera_opcion)
-    segunda_opcion=formatoImpresion(segunda_opcion)
-    tercera_opcion=formatoImpresion(tercera_opcion)
+    #primera_opcion=formatoImpresion(primera_opcion)
+    #segunda_opcion=formatoImpresion(segunda_opcion)
+    #tercera_opcion=formatoImpresion(tercera_opcion)
 
     pytesseract.pytesseract.tesseract_cmd = r'C:\Users\teto_\AppData\Local\Tesseract-OCR\tesseract.exe'
 
     titulo_string = pytesseract.image_to_string(titulo)
-    if titulo_string[0]=="é":
+    if titulo_string[0]=="é": 
         titulo_string=" "+titulo_string[1:-1]+" "
-
+        
     primera_string = pytesseract.image_to_string(primera_opcion)
     primera_string=primera_string.lower()
 
@@ -51,10 +56,55 @@ def procesar(direccion):
     #segunda_opcion.show()
     #tercera_opcion.show()
     #print(titulo_string+"\n"+primera_string+"\n"+segunda_string+"\n"+tercera_string)
-
+    
     pregunta = titulo_string
     respuestas = set()
     respuestas.add(primera_string)
     respuestas.add(segunda_string)
     respuestas.add(tercera_string)
     return pregunta, respuestas
+
+def procesar(I):
+    #I = I.convert("L")
+    I = formatoProcesamiento(I)
+    #I = I[280:890,42:500]
+    im = I[750:890,70:470]
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Users\teto_\AppData\Local\Tesseract-OCR\tesseract.exe'
+    I=formatoImpresion(I)
+    respuestas = pytesseract.image_to_string(I)
+    respuestas = respuestas.split("\n\n")
+    pregunta = respuestas.pop(0)
+    for i in range(len(respuestas)):
+        respuestas[i]=respuestas[i].lower()
+    if pregunta[0]=="é": 
+            pregunta=" "+pregunta[1:-1]+" "
+    #print(pregunta)
+
+    #print(respuestas)
+    return pregunta, respuestas
+
+def algo():  
+    I = Image.open("imagen1.jpeg")
+    #I.show()
+    I1 = I.crop((30,230,510,890))
+    #I.show()
+    #a = I.quantize(100,1,100)
+    #a.show()
+    #I1 = I.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    #I1 = I1.filter(ImageFilter.CONTOUR)
+    #I1 = I1.filter(ImageFilter.SMOOTH_MORE)
+    #I1 = I1.quantize(100,1,100)
+    #I1=I1.convert("CMYK")
+    tools=pyocr.get_available_tools()
+    if len(tools)==0:
+        print("No OCR tool found")
+        sys.exit(1)
+    print(tools)
+    tool = tools[0]
+    respuestas = tool.image_to_string(I1,lang="esp",builder = pyocr.builders.TextBuilder())
+    #respuestas = respuestas.split("\n\n")
+    if len(respuestas)<4:
+        I1 = I.crop((71,770,457,833))
+        respuestas = respuestas + pytesseract.image_to_string(I1)
+    print(respuestas)
+    I1.show()
