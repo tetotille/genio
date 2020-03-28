492045f0ec5b70534fa77803fea086f84099de47
tetotille <tetotille@gmail.com>
2020-03-28 12:04:11 -0400

    Cambié el print del final comentando un poco más

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ texto = definitivo.lower()
 contarCoincidencias(texto, lista_palabras)
 
 #########################################FIN########################################
-print("Tardó mucho", time()-start)
+print("Tardó mucho nomás", time()-start)
