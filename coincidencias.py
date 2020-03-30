dc8cc54f798149b0d8fa97094c4a6431c228c8ce
tetotille <tetotille@gmail.com>
2020-03-29 23:35:05 -0400

    Agregué separador de palabras

--- coincidencias.py
+++ coincidencias.py
@@ -1,13 +1,21 @@
 def contarCoincidencias(texto, lista_palabras):
     for pal in lista_palabras:
         b = 0
-        letras = len(pal)
         cont = -1
         acu = 0
-        pal = pal.lower()
+
         while b !=-1:
             b = texto[acu:len(texto)].find(pal)
             cont+=1
             acu+=b+1
-        print(pal+" = "+str(cont))
-        
+        print("\n\n", pal+" = "+str(cont))
+
+        cont = -1
+        for palabrita in pal.split():
+            b = 0
+            acu = 0
+            while b !=-1:
+                b = texto[acu:len(texto)].find(palabrita)
+                cont+=1
+                acu+=b+1
+        print("Palabras por separado =", cont)
